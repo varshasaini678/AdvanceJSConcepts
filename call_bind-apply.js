23dfdd6c3c3453e0e49a605bce554280d5fbe5f4
Varsha <varshasaini678@gmail.com>
2023-12-18 20:16:34 +0530

    call bind polufill

--- call_bind-apply.js
+++ call_bind-apply.js
@@ -25,7 +25,31 @@ const person2 = {
 //Call and apply
 
 person1.getFullName.call(person2,'New york','234234');
-
-
-
 person1.getFullName.apply(person2,['New york','234234']);
+
+
+Function.prototype.myCall = function(obj,...args){
+    obj.fnRef = this;//binding function to object
+    obj.fnRef(...args);
+}
+
+person1.getFullName.myCall(person2,'New york mycall','234234');
+
+Function.prototype.myApply = function(obj,args){
+    obj.fnRef = this;//binding function to object
+    obj.fnRef(...args);
+}
+
+person1.getFullName.myApply(person2,['New york myApply','234234']);
+
+
+Function.prototype.myBind = function(obj){
+    obj.fnRef=this;
+    
+    return function(...args){
+        obj.fnRef(...args)
+    }
+}
+
+const bindedFunction = person1.getFullName.bind(person2);
+bindedFunction();
